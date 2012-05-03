8b93672ab0400aaf02d0c75489d0ee22f54e961b
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2012-05-03 18:26:45 +0200

    aktualizacja AssemblyInfo

--- ReferenceArchiver/Properties/AssemblyInfo.cs
+++ ReferenceArchiver/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BdGui2")]
+[assembly: AssemblyTitle("Archiwizator Bibliografii")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BdGui2")]
+[assembly: AssemblyProduct("Archiwizator Bibliografii")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("pl-PL")]
