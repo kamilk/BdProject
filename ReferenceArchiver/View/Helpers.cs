260e304b5ddd6a80e016d62cf4ea025b55f9873c
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2012-05-03 10:59:31 +0200

    teraz filtrowanie powinno działać :)

---
+++ ReferenceArchiver/View/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace ReferenceArchiver.View
+{
+    static class Helpers
+    {
+        public static void UpdateTextBinding(this TextBox textBox)
+        {
+            textBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        }
+
+    }
+}
