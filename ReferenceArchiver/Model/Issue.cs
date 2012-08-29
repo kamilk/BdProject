16423a0a5cb66c2f9078c868f2958d43cf421849
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2012-08-29 22:27:40 +0200

    Uzupełnienie modelu Issue.

--- ReferenceArchiver/Model/Issue.cs
+++ ReferenceArchiver/Model/Issue.cs
@@ -10,5 +10,25 @@ namespace ReferenceArchiver.Model
     /// </summary>
     class Issue
     {
+        public int InstitutionId;
+        public int PublisherId;
+        public int JournalId;
+        public int IdWithinJournal;
+
+        public int NumberWithinJournal;
+        public int NumberWithinPublisher;
+
+        public string Title;
+        public int YearOfPublishing;
+        public bool WasVerified;
+        public string Type;
+        public string TypeNumber;
+
+        public Issue(ResearchJournal parentJournal)
+        {
+            InstitutionId = parentJournal.InstitutionId;
+            PublisherId = parentJournal.PublisherId;
+            JournalId = parentJournal.IdWithinPublisher;
+        }
     }
 }
